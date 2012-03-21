b1b8b7290bba265c2ae9e537157078d4a4eab15c
Patrick Hesse <blackhawkdc@sumbalum.de>
2012-03-21 14:19:46 +0100

    Use Cases als Tests
    die Use Cases aus dr Readme.txt in die TestKlasse UseCases übertragen

--- test/com/phesse/decisionTable/UseCases.java
+++ test/com/phesse/decisionTable/UseCases.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class UseCases {
-
 	/**
 	    ------------|-------|-------|
 					| R1	| 	R2  |
@@ -18,6 +17,93 @@ public class UseCases {
 		------------|-------|-------|
 	 */
 	@Test public void useCase1() {
-	}
+		/********
+		** Code zum Aufbau der Tabelle für Beispiel 1:
+		********/
+		Condition condition = new Condition("is_young", true);
+		Rule rule1 = new Rule(	new Condition[]{condition}, new String[]{"Action_1"});
 
+		Condition condition2 = new Condition("is_young", false);
+		Rule rule2 = new Rule(new Condition[]{condition2},	new String[]{"Action_2"});
+
+		DecisionTable decisionTable = new DecisionTable();
+		decisionTable.addRule(rule1);
+		decisionTable.addRule(rule2);
+
+		/********
+		** Code zum Benutzen der Tabelle für Beispiel 1:
+		********/
+		Fact fact = new Fact();
+		fact.setValue("is_young", true);
+		String[] actions = (String[]) decisionTable.execute(fact); 
+		assertArrayEquals(new String[]{"Action_1"}, actions);
+
+		fact = new Fact();
+		fact.setValue("is_young", false);
+		actions = (String[]) decisionTable.execute(fact);
+		assertArrayEquals(new String[]{"Action_2"}, actions);
+	}
+	
+	/**
+		------------|-------|-------|-------|
+					| R1	| 	R2  | 	R2  |
+		-------------------------------------
+		is_young    | true  | true  | false |
+		------------|-------|-------|--------
+		need_money  | true  | false | -     | 
+		------------|-------|-------|--------
+		Action_1    | x     |       |       | 
+		------------|-------|-------|--------
+		Action_2    |       | x     | x     |
+		------------|-------|-------|--------
+		Action_3    |       |       | x     |
+		------------|-------|-------|--------
+	 */
+	@Test public void useCase2() {
+		// Aufbau der Tabelle 
+		Rule rule1 = new Rule(
+			new Condition[]{
+				new Condition("is_young", true),
+				new Condition("need_money", true)
+			},	
+			new String[]{"Action_1"}
+		);
+
+		Rule rule2 = new Rule(
+			new Condition[]{
+				new Condition("is_young", true),
+				new Condition("need_money", false)
+			},	
+			new String[]{"Action_2"}
+		);
+		Rule rule3 = new Rule(
+			new Condition[]{
+				new Condition("is_young", false)
+			},	
+			new String[]{"Action_2", "Action_3"}
+		);
+
+		DecisionTable decisionTable = new DecisionTable();
+		decisionTable.addRule(rule1);
+		decisionTable.addRule(rule2);
+		decisionTable.addRule(rule3);
+
+		//Benutzen der Tabelle 
+		Fact fact = new Fact();
+		fact.setValue("is_young", true);
+		fact.setValue("need_money", true);
+		String[] actions = (String[]) decisionTable.execute(fact);
+		assertArrayEquals(new String[]{"Action_1"}, actions);
+
+		fact = new Fact();
+		fact.setValue("is_young", true);
+		fact.setValue("need_money", false);
+		actions = (String[]) decisionTable.execute(fact);
+		assertArrayEquals(new String[]{"Action_2"}, actions);
+
+		fact = new Fact();
+		fact.setValue("is_young", false);
+		actions = (String[]) decisionTable.execute(fact); 
+		assertArrayEquals(new String[]{"Action_2", "Action_3"}, actions);
+	}
 }
